47c0cb5223f52a32b8463af2828c511541d71f1b
Avishek Roy <roy1807053@stud.kuet.ac.bd>
2022-07-26 06:33:24 +0600

    adapter

--- app/src/main/java/com/example/myapplication/ModelClassList.java
+++ app/src/main/java/com/example/myapplication/ModelClassList.java
@@ -6,7 +6,45 @@ import java.util.List;
 
 
 
-    public class ModelClassList {
+public class ModelClassList {
+    private List<ModelClass> records = new ArrayList<>();
 
+    /**
+     * Adding a record in the list
+     * @param record
+     *  ADDS THE RECORD
+     *
+     */
+    public void addRecord (ModelClass record) {
+        if(records.contains(record)) {
+            throw new IllegalArgumentException();
+        } else {
+            records.add(record);
+        }
     }
+
+    /**
+     * Deletes an existing RECORD to the list
+     * @param record
+     *      DELETES THE RECORD
+     */
+    public void deleteRecord (ModelClass record) {
+        if(records.contains(record)) {
+            records.remove(record);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * This method is for returning a sorted list
+     * @return
+     *      returns a sorted list of records
+     */
+    public List<ModelClass> getRecords() {
+        List<ModelClass> recordList = records;
+        Collections.sort(recordList);
+        return recordList;
+    }
+}
 
