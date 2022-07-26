2a77d934733ac242f8a343fca3df0e421f924754
Avishek Roy <roy1807053@stud.kuet.ac.bd>
2022-07-26 06:40:19 +0600

    Activity main, insert

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -30,9 +30,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        insertButton = findViewById(R.id.insertButton);
+        retrieveData();
+        modelClass = new ModelClass("jjdj","ddjd",10,20,40,"hellow");
+        //arrayList.add(modelClass);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerAdapter = new RecyclerAdapter(this,arrayList);
+        recyclerView.setAdapter(recyclerAdapter);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerAdapter.setCustomClickListener(new RecyclerAdapter.CustomClickListener() {
+            @Override
+            public void cusOnClick(int position, View v) {
+                Intent intent = new Intent(MainActivity.this, ViewDetailsActivity.class);
+                intent.putExtra("index",position);
+                startActivity(intent);
+            }
+            @Override
+            public void onDeleteClick(int position) {
+                arrayList.remove(position);
+                recyclerAdapter.notifyItemRemoved(position);
+                saveData();
+                Toast.makeText(MainActivity.this,"Deleted successful!",Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onEditClick(int position) {
 
- }
+                Intent intent = new Intent(MainActivity.this, UpdateActivity.class);
+                intent.putExtra("index",position);
+                startActivity(intent);
+            }
 
+        });
+        insertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,InsertActivity.class));
+            }
 
+        });
+
+    }
+    private void saveData()
+    {
+        sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        gson = new Gson();
+        String jsonString = gson.toJson(arrayList);
+        editor.putString("record",jsonString);
+        editor.apply();
+    }
+    private void retrieveData()
+    {
+        sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        gson = new Gson();
+        String jsonString = sharedPreferences.getString("record",null);
+        Type type = new TypeToken<ArrayList<ModelClass>>(){}.getType();
+        arrayList = gson.fromJson(jsonString,type);
+        if(arrayList ==null)
+        {
+            arrayList = new ArrayList<ModelClass>();
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/ModelClass.java
+++ app/src/main/java/com/example/myapplication/ModelClass.java
@@ -2,5 +2,85 @@ package com.example.myapplication;
 
 public class ModelClass implements Comparable<ModelClass>{
 
+    private String date="";
+    private String time="";
+    private int systolic=0;
+    private int diastolic=0;
+    private int heartRate=0;
+    private String comment="";
+
+
+    public ModelClass(String date, String time, int systolic, int diastolic, int heart_rate, String comment) {
+        this.date = date;
+        this.time = time;
+        this.systolic = systolic;
+        this.diastolic = diastolic;
+        this.heartRate = heart_rate;
+        this.comment = comment;
+    }
+
+    public ModelClass(String date, String time, int systolic, int diastolic, int heart_rate) {
+        this.date = date;
+        this.time = time;
+        this.systolic = systolic;
+        this.diastolic = diastolic;
+        this.heartRate = heart_rate;
+        this.comment = "";
+    }
+
+
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getSystolic() {
+        return systolic;
+    }
+
+    public int getDiastolic() {
+        return diastolic;
+    }
+
+    public int getHeartRate() {
+        return heartRate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setSystolic(int systolic) {
+        this.systolic = systolic;
+    }
+
+    public void setDiastolic(int diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public void setHeart_rate(int heart_rate) {
+        this.heartRate = heart_rate;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public int compareTo(ModelClass record) {
+        return this.date.compareTo(record.getDate());
+    }
 
 }
